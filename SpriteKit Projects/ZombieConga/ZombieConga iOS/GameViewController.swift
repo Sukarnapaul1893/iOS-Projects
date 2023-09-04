e7919008a4aab60a34308ee21803d5092c573f28
Sukarna Paul <sukarnapaul@Sukarnas-Mac-mini.local>
2023-09-04 22:41:44 +0600

    FirstSpriteKitProject

---
+++ SpriteKit Projects/ZombieConga/ZombieConga iOS/GameViewController.swift
@@ -0,0 +1,29 @@
+//
+//  GameViewController.swift
+//  ZombieConga iOS
+//
+//  Created by Sukarna Paul on 9/4/23.
+//
+
+import UIKit
+import SpriteKit
+import GameplayKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        if UIDevice.current.userInterfaceIdiom == .phone {
+            return .allButUpsideDown
+        } else {
+            return .all
+        }
+    }
+
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+}
