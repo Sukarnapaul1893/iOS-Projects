35c740c6211ad4b8209db053ddcaaa880f833ba5
Sukarna Paul <sukarnapaul@Sukarnas-Mac-mini.local>
2023-09-07 01:52:38 +0600

    TextEditorAdd

--- CalculatorApp/CalculatorApp/ContentView.swift
+++ CalculatorApp/CalculatorApp/ContentView.swift
@@ -8,13 +8,26 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var textEditor: String = ""
+    @State var textEditor: String = "This is the starting text"
     var body: some View {
         NavigationView {
             VStack {
-                 TextEditor(text: $textEditor)
+                TextEditor(text: $textEditor)
+                    .frame(height: 250)
+                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                    Text("Save".uppercased())
+                        .font(.headline)
+                        .foregroundColor(.white)
+                        .padding()
+                        .frame(maxWidth: .infinity)
+                        .background(Color.blue)
+                        .cornerRadius(10)
+                })
             }
-        }
+            .padding()
+            .background(Color.green)
+            .navigationTitle("Calculator App")
+         }
     }
 }
 
