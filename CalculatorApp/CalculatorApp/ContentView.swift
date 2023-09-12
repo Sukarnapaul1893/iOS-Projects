78405a044f05cbfe0c7f63f23d93eed304653c50
Sukarna Paul <sukarnapaul@Sukarnas-Mac-mini.local>
2023-09-12 22:26:34 +0600

    addedTextFieldForNumber

--- CalculatorApp/CalculatorApp/ContentView.swift
+++ CalculatorApp/CalculatorApp/ContentView.swift
@@ -5,16 +5,30 @@
 //  Created by Sukarna Paul on 9/6/23.
 //
 
+import Combine
 import SwiftUI
 
 struct ContentView: View {
-    @State var textEditor: String = "This is the starting text"
+    @State var textEditorText: String = "This is the starting text"
+    @State var savedText: String = ""
+    @State private var numOfPeople = "0"
     var body: some View {
         NavigationView {
             VStack {
-                TextEditor(text: $textEditor)
-                    .frame(height: 250)
-                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                TextField("Total number of people", text: $numOfPeople)
+                    .keyboardType(.numberPad)
+                    .onReceive(Just(numOfPeople)) { newValue in
+                        let filtered = newValue.filter { "0123456789".contains($0) }
+                        if filtered != newValue {
+                            self.numOfPeople = filtered
+                        }
+                    }
+                    .frame(height: 40)
+                    .background(.white)
+                    
+                Button(action: {
+                    savedText = numOfPeople
+                }, label: {
                     Text("Save".uppercased())
                         .font(.headline)
                         .foregroundColor(.white)
@@ -23,6 +37,9 @@ struct ContentView: View {
                         .background(Color.blue)
                         .cornerRadius(10)
                 })
+                Text(savedText).multilineTextAlignment(.trailing)
+                Spacer()
+                
             }
             .padding()
             .background(Color.green)
