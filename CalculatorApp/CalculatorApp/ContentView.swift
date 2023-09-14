73d317398b223e45054739f4f81585f5edab92db
Sukarna Paul <sukarnapaul@Sukarnas-Mac-mini.local>
2023-09-14 08:09:02 +0600

    TextEditor

--- CalculatorApp/CalculatorApp/ContentView.swift
+++ CalculatorApp/CalculatorApp/ContentView.swift
@@ -25,6 +25,16 @@ struct ContentView: View {
                     }
                     .frame(height: 40)
                     .background(.white)
+                TextField("Total number of people", text: $numOfPeople)
+                    .keyboardType(.numberPad)
+                    .onReceive(Just(numOfPeople)) { newValue in
+                        let filtered = newValue.filter { "0123456789".contains($0) }
+                        if filtered != newValue {
+                            self.numOfPeople = filtered
+                        }
+                    }
+                    .frame(height: 40)
+                    .background(.white)
                     
                 Button(action: {
                     savedText = numOfPeople
