bb00acba7a8cf553909a28dae2c3efd6327bb397
Sukarna Paul <sukarnapaul@Sukarnas-Mac-mini.local>
2023-10-05 01:39:47 +0600

    LateCounter

---
+++ LateCounter/LateCounter/LateCounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  LateCounterApp.swift
+//  LateCounter
+//
+//  Created by Sukarna Paul on 10/4/23.
+//
+
+import SwiftUI
+
+@main
+struct LateCounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
